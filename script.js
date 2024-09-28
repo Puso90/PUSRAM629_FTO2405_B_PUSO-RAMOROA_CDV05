60cfd2b61784871b925445be7eee395af377c347
Puso Ramoroa <43412934+Puso90@users.noreply.github.com>
2024-09-28 13:42:36 +0200

    Personal website and portfolio

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const menu = [
+  {	home: 
+  	["boom the holy grail"]
+  	["you've reached the kingdom"]
+  	["okay, okay you've been here a while..."]
+  	["hmm. what are you looking for?"] 
+  },
+  
+  {	work: 
+  	["look up, lucky lotto numbers"]
+    ["yourcater, nothing less than exquisite"]
+    ["liberated produce, manufacturing, supply"]
+    ["reagile farming, cultivating greatness"]
+    ["gasp"]
+  },
+]
+
